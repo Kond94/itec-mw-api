ac813df2a41c0b1f1ff7912d576ea31980e5de0d
Kondwerani Kamsesa <kamsesakond@outlook.com>
2023-09-27 07:55:41 +0200

    x

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
-  res.setHeader("Access-Control-Request-Headers", "x-requested-with"); //according jquery docs it only works with this header.
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  next();
   next();
 });
 
